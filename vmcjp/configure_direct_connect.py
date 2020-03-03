16f0a53ac8a588ad464d7dade71d4faf3f808bab
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-03 21:09:33 +0900

    Update configure_direct_connect.py

--- vmcjp/configure_direct_connect.py
+++ vmcjp/configure_direct_connect.py
@@ -4,7 +4,6 @@ import requests
 import atexit
 
 from com.vmware.nsx_vmc_app_client_for_vmc import create_nsx_vmc_app_client_for_vmc
-from vmcjp.utils.metadata import get_members
 
 def get_nsx_app_client(token, org_id, sddc_id):
   session = requests.Session()
@@ -29,12 +28,11 @@ def main():
   )
 
   print(get_members(nsx_app_client.infra.direct_connect.Vifs.list()))
-#  print(nsx_app_client.infra.direct_connect.Vifs.list().to_json())
-#  print(nsx_app_client.infra.direct_connect.Vifs.list().get_field())
 
-#  count = nsx_app_client.infra.direct_connect.Vifs.list().result_count
   vifs = nsx_app_client.infra.direct_connect.Vifs.list().results
-  for vif in vifs: #we will attach all available VIFs to SDDC.
+  
+  #we will attach all available VIFs to SDDC.
+  for vif in vifs:
     vifs.create(vif.id, "ATTACH")
 
 if __name__ == '__main__':
