5bdb5d20c36803f51d434ca9aad0275dfc549b50
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-03 14:49:14 +0900

    Update get_firewall_rule.py

--- vmcjp/get_firewall_rule.py
+++ vmcjp/get_firewall_rule.py
@@ -27,10 +27,44 @@ def get_nsx_policy_client(token, org_id, sddc_id):
   return nsx_policy_client
 
 def list_firewall_rules(nsx_client):
-  policies = nsx_client.infra.domains.GatewayPolicies.get("cgw", "default")
+  geteway_type = "cgw" #or "mgw"
+  security_groups = get_security_group_ids_and_names(gateway_type, nsx_client)
+  policies = nsx_client.infra.domains.GatewayPolicies.get(geteway_type, "default")
   rules = policies.get_field("rules")
-  print(rules)
-    
+  
+  rules_list = [
+    get_rules(rule, gateway_type, security_groups) 
+    for rule in rules
+  ]
+  
+  print(rules_list)
+  
+def get_security_group_ids_and_names(gateway_type, nsx_client):
+  security_groups = nsx_client.infra.domains.Groups.list(gateway_type).results
+  return {sg.get_field("id"):sg.get_field("display_name") for sg in security_groups}
+  
+def get_rules(rule, gateway_type, security_groups):
+  sn = rule.get_field("sequence_number")
+  source_groups = rule.get_field("source_groups")
+  sg = replace_strings_in_list(source_groups, "/infra/domains/" + gateway_type + "/groups/")
+  sg_names = compare_list_and_dict(sg, security_groups)
+  dest_groups = rule.get_field("destination_groups")
+  dg = replace_strings_in_list(dest_groups, "/infra/domains/" + gateway_type + "/groups/")
+  dg_names = compare_list_and_dict(dg, security_groups)
+  
+  return {"create_user": rule.get_field("create_user"),
+          "display_name": rule.get_field("display_name"),
+          "logged": rule.get_field("logged"),
+          "destination_groups": dest_groups,
+          "destination_groups_names": dg_names,
+          "scope": rule.get_field("scope"),
+          "services": rule.get_field("services"),
+          "sequence_number": sn,
+          "action": rule.get_field("action"),
+          "source_groups": source_groups,
+          "source_group_names": sg_names
+         }
+  
 def main():
   token = "VMC_Refresh_Token_xxxxxxx"
   org_id = "VMC Org ID"
