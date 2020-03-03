715c078236c439306e421709204557f5cabb3f84
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-03 14:53:58 +0900

    Update get_firewall_rule.py

--- vmcjp/get_firewall_rule.py
+++ vmcjp/get_firewall_rule.py
@@ -67,6 +67,11 @@ def get_rules(rule, gateway_type, security_groups):
           "source_group_names": sg_names
          }
   
+def compare_list_and_dict(ls, dic):
+  key_list = dic.keys()
+  dup_list = set(key_list) & set(ls)
+  return [dic[id] for id in dup_list]
+
 def main():
   token = "VMC_Refresh_Token_xxxxxxx"
   org_id = "VMC Org ID"
