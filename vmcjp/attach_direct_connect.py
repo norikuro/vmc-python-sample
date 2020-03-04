f0d67e56233b7b84ea9550be256fdd1551f63fd8
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-04 15:16:32 +0900

    Update attach_direct_connect.py

--- vmcjp/attach_direct_connect.py
+++ vmcjp/attach_direct_connect.py
@@ -26,18 +26,51 @@ def attach_vifs(vifs):
     except Exception as e:
       print("Failed to attach VIF, {}".format(e.message))
 
-def check_attached_vif_status(vif):
+def check_attached_vif_status(vifs):
   #wait 3 minuites until attached VIF status will be available.
   wait_time = 3 * 60 
   
   start = time.time()
-    while True:
+  while True:
     elapsed_time = time.time() - start
     if elapsed_time > wait_time:
       return
     else:
-      checkaaa
-    
+      for vif in vifs:
+        vif_id = vif.id
+        vif_state = vif.state
+        print("cheking VIF of id:{} status".format(vif_id))
+        
+        if vif_state == "AVAILABLE":
+          Print("VIF of id:{} is available!".format(vif_id))
+          return
+        else:
+          Print("VIF of id:{} is {}".format(vif_id, vif_state))
+          #sleep 30 secs
+          sleep(30)
+
+def check_attached_vif_bgp_status(vifs):
+  #wait 3 minuites until attached VIF status will be available.
+  wait_time = 3 * 60 
+  
+  start = time.time()
+  while True:
+    elapsed_time = time.time() - start
+    if elapsed_time > wait_time:
+      return
+    else:
+      for vif in vifs:
+        vif_id = vif.id
+        vif_bgp_state = vif.bgp_status
+        print("cheking VIF of id:{} BGP status".format(vif_id))
+        
+        if vif_bgp_state == "UP":
+          Print("VIF of id:{} BGP status is UP!".format(vif_id))
+          return
+        else:
+          Print("VIF of id:{} BGP status is {}".format(vif_id, vif_bgp_state))
+          #sleep 30 secs
+          sleep(30)
 
 def main():
   token = "VMC_Refresh_Token_xxxxxxx"
